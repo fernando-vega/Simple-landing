c2bc4e2486b2feb5044c38126487d20c231cbeaf
Fernando Vega <juanfe.vega@gmail.com>
2019-02-12 12:10:36 -0500

    send message

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,55 @@
-const mv = new Vue({
-  el: "#form",
-  data: {
-    formType: true,
-    message: {
-      name: "",
-      email: "",
-      phone: ""
-    }
-  }
+$(document).ready(function(){
+  $('#send_message').click(function(e){
+      
+      //Stop form submission & check the validation
+      e.preventDefault();
+      
+      // Variable declaration
+      var error = false;
+      var name = $('#name').val();
+      var email = $('#email').val();
+      var phone = $('#phone').val();
+      
+     // Form field validation
+      if(name.length == 0){
+          var error = true;
+          $('#name_error').fadeIn(500);
+      }else{
+          $('#name_error').fadeOut(500);
+      }
+      if(email.length == 0 || email.indexOf('@') == '-1'){
+          var error = true;
+          $('#email_error').fadeIn(500);
+      }else{
+          $('#email_error').fadeOut(500);
+      }
+      if(phone.length == 0){
+          var error = true;
+          $('#phone_error').fadeIn(500);
+      }else{
+          $('#phone_error').fadeOut(500);
+      }
+      
+      // If there is no validation error, next to process the mail function
+      if(error == false){
+         // Disable submit button just after the form processed 1st time successfully.
+          $('#send_message').attr({'disabled' : 'true', 'value' : 'Enviando...' });
+          
+  /* Post Ajax function of jQuery to get all the data from the submission of the form as soon as the form sends the values to email.php*/
+          $.post("../send.php", $("#formHeader").serialize(),function(result){
+              //Check the result set from email.php file.
+              if(result == 'sent'){
+                  //If the email is sent successfully, remove the submit button
+                   $('#send_message').remove();
+                  //Display the success message
+                  $('#mail_success').fadeIn(500);
+              }else{
+                  //Display the error message
+                  $('#mail_fail').fadeIn(500);
+              }
+          });
+      }
+  });    
 });
-
 AOS.init();
-https://www.youtube.com/watch?v=MOVJbEUk5CY&list=PLtTGWoGMH2rO7A40ZuSVT5eN35JLyMWf5&index=6
+
---
+++ build/send.php
@@ -0,0 +1,25 @@
+<?php
+$emailTo = 'juanfe.vega@hotmail.com';  //Recipient's E-mail
+$subject = "Correo envíado desde la página web";
+$name =  $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$phone = $_REQUEST['phone'];
+
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+$message = 'Nombre : ' . $name . "<br/>";
+$message .= 'Correo Electrónico : ' . $email . "<br/>";
+$message .= 'Telénfono : ' . $phone;
+
+if (@mail($emailTo, $subject, $message, $headers, "-r".$emailTo))
+{
+     // Transfer the value 'sent' to ajax function for showing success message.
+     echo 'sent';
+}
+else
+{
+     // Transfer the value 'failed' to ajax function for showing error message.
+     echo 'failed';
+}
+?>
