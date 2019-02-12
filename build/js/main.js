d4fb44d7f66f68d016d2b695afda8ef1c40cffa9
Fernando Vega <juanfe.vega@gmail.com>
2019-02-12 07:44:33 -0500

    base form

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,14 @@
+const mv = new Vue({
+  el: "#form",
+  data: {
+    formType: true,
+    message: {
+      name: "",
+      email: "",
+      phone: ""
+    }
+  }
+});
+
 AOS.init();
+https://www.youtube.com/watch?v=MOVJbEUk5CY&list=PLtTGWoGMH2rO7A40ZuSVT5eN35JLyMWf5&index=6
